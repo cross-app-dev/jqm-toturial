e1bff1754f4506ad2b68ac1fbc03c399bd126567
Wael Showair <show0017@algonquinlive.com>
2014-11-17 23:55:44 -0500

    initial mobile web app

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*************************
+Checklist of app delivery:
+
+1. The home page for the app should provide some basic information about what the tutorial topic is and a button to start the tutorial.
+
+2. info/about page that provides your personal info as the developer.
+
+3. links to the home page and info page should be presented as two buttons that fill a fixed footer at the bottom of the screen.
+
+4.
+
+Useful attributes:
+data-role=page, page-dialog=true,
+page-tranisition=flip/slide
+to buttons in the footer, use class ui-btn-right or ui-btn
+apply class ui-bar to the footer it gives more spaces instead of crowded elements in the footer.
+Search for ui-btn-icon
+ui-mini, ui-btn-inline or u can use controlgroup instead and set data-type attribute to horizontal or vertical
+buttons with icons: ui-btn-arrow-u/l/r/d ui-btn-icon-top/bottom/left/right ui-corner-all ui-btn-notext
+**************************/
